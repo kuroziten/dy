f34a3b9454417a5c73a0566f98ce2668d375b885
kuroziten <33644935+kuroziten@users.noreply.github.com>
2022-03-12 00:14:34 +0900

    高速化？２

--- YouTubeとニコニコ動画をiframeで見るやつ.js
+++ YouTubeとニコニコ動画をiframeで見るやつ.js
@@ -2,50 +2,49 @@
  * 原案：NOT
  * 作成者：国語辞典、NOT
  * 改修者：noname
+ * 改修案者：NOT、Kot
  */
-const drrrkot_observer = new MutationObserver((mutations) => {
+const drrrjit_observer = new MutationObserver((mutations) => {
   mutations.forEach((mutation) => {
-window.setInterval(() => {
-  const bodies = document.querySelectorAll(".body");
-  const youTubeURLs = ["https://www.youtube.com/watch?v=", "https://youtu.be/","https://youtube.com/shorts/", "https://m.youtube.com/watch?v="];
-  const nicoVideoURLs = ["https://www.nicovideo.jp/watch/", "https://nico.ms/"];
-  /* 複数のpatternのどれかを含むか確認する関数 */
-  const includes = (text, patterns) =>
-    patterns.some((pattern) => text.includes(pattern));
-  /* 複数のpatternを削除する関数 */
-  const removePatterns = (text, patterns) =>
-    patterns.reduce((text, pattern) => text.replace(pattern, ""), text);
-  /* テキストを埋め込みURLに変換する関数 */
-  /* 変換できないテキストが渡されたときはundefinedを返す */
-  const toEmbeddedURL = (text) =>
-    includes(text, youTubeURLs)
-      ? "https://www.youtube.com/embed/" + removePatterns(text, youTubeURLs)
-      : includes(text, nicoVideoURLs)
-      ? "https://embed.nicovideo.jp/watch/" +
-        removePatterns(text, nicoVideoURLs)
-      : undefined;
-  const isSmartPhone = navigator.userAgent.match(/iPhone|Android.+Mobile/);
-  for (const body of bodies) {
-    const embeddedURL = toEmbeddedURL(body.innerText);
-    if (!embeddedURL) continue;
-    const iframe = document.createElement("iframe");
-    iframe.setAttribute("width", isSmartPhone ? "200" : "440");
-    iframe.setAttribute("height", isSmartPhone ? "112.5" : "247.5");
-    iframe.setAttribute("src", embeddedURL);
-    body.innerText = "";
-    body.style.backgroundSize = "cover";
-    body.appendChild(iframe);
-    /* 動画ロード後に要素のサイズを変更する */
-    setTimeout(() => {
-      body.style.width = "";
-      body.style.height = "";
-    }, 500);
-  }
-}, 500);
-});
+    const bodies = document.querySelectorAll(".body");
+    const youTubeURLs = ["https://www.youtube.com/watch?v=", "https://youtu.be/","https://youtube.com/shorts/", "https://m.youtube.com/watch?v="];
+    const nicoVideoURLs = ["https://www.nicovideo.jp/watch/", "https://nico.ms/"];
+    /* 複数のpatternのどれかを含むか確認する関数 */
+    const includes = (text, patterns) =>
+      patterns.some((pattern) => text.includes(pattern));
+    /* 複数のpatternを削除する関数 */
+    const removePatterns = (text, patterns) =>
+      patterns.reduce((text, pattern) => text.replace(pattern, ""), text);
+    /* テキストを埋め込みURLに変換する関数 */
+    /* 変換できないテキストが渡されたときはundefinedを返す */
+    const toEmbeddedURL = (text) =>
+      includes(text, youTubeURLs)
+        ? "https://www.youtube.com/embed/" + removePatterns(text, youTubeURLs)
+        : includes(text, nicoVideoURLs)
+        ? "https://embed.nicovideo.jp/watch/" +
+          removePatterns(text, nicoVideoURLs)
+        : undefined;
+    const isSmartPhone = navigator.userAgent.match(/iPhone|Android.+Mobile/);
+    for (const body of bodies) {
+      const embeddedURL = toEmbeddedURL(body.innerText);
+      if (!embeddedURL) continue;
+      const iframe = document.createElement("iframe");
+      iframe.setAttribute("width", isSmartPhone ? "200" : "440");
+      iframe.setAttribute("height", isSmartPhone ? "112.5" : "247.5");
+      iframe.setAttribute("src", embeddedURL);
+      body.innerText = "";
+      body.style.backgroundSize = "cover";
+      body.appendChild(iframe);
+      /* 動画ロード後に要素のサイズを変更する */
+      setTimeout(() => {
+        body.style.width = "";
+        body.style.height = "";
+      }, 500);
+    }
+  });
 });
 const config = {childList:true,subtree:true};
-drrrkot_observer.observe(document.body, config);
+drrrjit_observer.observe(document.body, config);
 
 
 
